e154d3c53ae136a41d02b4a684ec62fb62773ac0
shariqanwar20 <shariq.anwar@nixorcollege.edu.pk>
2021-07-22 17:24:18 +0500

    added a header component header of different pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from '../components/Navbar' 
 import Card from '../components/Card'
-import githubLogo from '../images/github-logo.png'
+import Header from '../components/Header'
+// import githubLogo from '../images/github-logo.png'
 
 export default () => {
     return (
         <>
             <Navbar />
+            {/* <Header title="Web Development" description="Lorem Ipsum is simply dummy text of the printing typesetting industry. simply dummy"/> */}
             {/* <Card imgSrc={githubLogo} title="Web Development" description="Lorem Ipsum is simply dummy text of the printing typesetting industry. simply dummy"/> */}
         </>
     );
